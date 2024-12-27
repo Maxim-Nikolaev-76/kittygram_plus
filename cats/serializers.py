3284cdbd6faca6b3488e3acc36eca71fc9275bba
Maxim <zlodey000@mail.ru>
2024-12-27 22:48:02 +0500

    lesson 13/15

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,87 @@
+import datetime as dt
+import webcolors
 from rest_framework import serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
 
-from .models import Cat
 
-
-class CatSerializer(serializers.ModelSerializer):
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color')
+
+
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'name')
+
+
+class CatSerializer(serializers.ModelSerializer):
+    # owner = serializers.StringRelatedField(read_only=True)
+    # Óбрали read_only=True
+    achievements_name = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    # color = Hex2NameColor()
+    color = serializers.ChoiceField(choices=CHOICES)
+
+    class Meta:
+        model = Cat
+        fields = (
+            'id',
+            'name',
+            'color',
+            'birth_year',
+            'owner',
+            'achievements_name',
+            'age'
+        )
+
+    @action(detail=False, url_name='recent-white-cats')
+    def recent_white_cats(self, request):
+        cats = Cat.objects.filter(color='White')[:5]
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
+
+    def create(self, validated_data):
+        if 'achievements' not in self.initial_data:
+            cat = Cat.objects.create(**validated_data)
+            return cat
+        achievements = validated_data.pop('achievements')
+        cat = Cat.objects.create(**validated_data)
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement
+            )
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat
+            )
+        return cat
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,30 @@
 from rest_framework import viewsets
-
-from .models import Cat
-from .serializers import CatSerializer
+from rest_framework import mixins
+from .models import Cat, Owner
+from .serializers import CatSerializer, CatListSerializer, OwnerSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CatListSerializer
+        return CatSerializer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+
+class CreateRetrieveViewSet(mixins.CreateModelMixin,
+                            mixins.RetrieveModelMixin,
+                            viewsets.GenericViewSet):
+    pass
+
+
+class LightCatViewSet(CreateRetrieveViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
